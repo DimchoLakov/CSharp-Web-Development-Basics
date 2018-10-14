8c391f65f1154837360b425324d141cf5889b9c8
whywherewhat <mdsektor@gmail.com>
2018-10-14 22:05:40 +0100

    Fixed small bugs

--- SIS/IRunes.Data/Configs/Configuration.cs
+++ SIS/IRunes.Data/Configs/Configuration.cs
@@ -3,6 +3,6 @@
     public class Configuration
     {
         public const string ConnectionString =
-            @"Server=DESKTOP-2G28R6E\SQLEXPRESS01;Database=IRunes;Integrated Security=True";
+            @"Server=.;Database=IRunes;Integrated Security=True";
     }
 }
