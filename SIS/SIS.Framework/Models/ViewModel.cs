5a14de713244dd708575fe3132f97c67055c715f
whywherewhat <mdsektor@gmail.com>
2018-10-21 21:33:06 +0100

    Introduced Model and ViewModel classes. Added ModelState functionality

---
+++ SIS/SIS.Framework/Models/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SIS.Framework.Models
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.Data = new Dictionary<string, object>();
+        }
+
+        public Dictionary<string, object> Data { get; set; }
+
+        public object this[string key]
+        {
+            get { return this.Data[key];}
+            set { this.Data[key] = value; }
+        }
+    }
+}
