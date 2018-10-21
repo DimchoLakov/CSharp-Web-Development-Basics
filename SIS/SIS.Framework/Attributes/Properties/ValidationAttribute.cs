b6a1fb6a067eac38e798b40971003bde9bc0fb80
whywherewhat <mdsektor@gmail.com>
2018-10-21 21:32:25 +0100

    Introduced Validation Attributes

---
+++ SIS/SIS.Framework/Attributes/Properties/ValidationAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SIS.Framework.Attributes.Properties
+{
+    public abstract class ValidationAttribute : Attribute
+    {
+        public abstract bool IsValid(object value);
+    }
+}
